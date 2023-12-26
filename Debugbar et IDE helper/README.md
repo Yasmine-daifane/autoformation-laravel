ef5f6457f4b5a58919c87c564158d5e81361ab89
yasmine <110662044+Yasmine-daifane@users.noreply.github.com>
2023-12-26 09:33:32 +0100

    add debug

---
+++ Debugbar et IDE helper/README.md
@@ -0,0 +1,4 @@
+# model binding
+
+- dans l'Url on a un slug et une Id  dans controler on le recupere l'article qui correspond laravel a la possibilite  de fair ca dynamiquement
+-  
---
+++ Debugbar et IDE helper/lab-crud-laravel-autoformation/resources/views/blog/index.blade.php
@@ -0,0 +1,33 @@
+{{--  use the template  base and fill the section name content  with h1 as a content  --}}
+{{--  lesaccolades font de l'echapement pour eviter le probleme d'injection   --}}
+{{--  des recourcis @foreach($variable as $key => $value) @endforeach  --}}
+{{--  methode magique links() permet de generer des liens --}}
+@extends('base') 
+@section('title', 'hi its me')
+
+
+    @section('content')
+    <h1>mon blog y here</h1>
+  
+        
+  
+
+
+ @dump($posts)
+
+ @foreach($posts as $post )
+    <article >
+        <h2> {{ $post->title }}</h2>
+        <p>
+            {{ $post->content }}
+        </p>
+        <p>  
+            <a href="{{route ('blog.show',['slug'=>$post ->slug ,'post'=>$post->id]) }}" class="btn btn-primary">lire la suite </a>
+        </p>
+    </article>
+ @endforeach
+
+  {{$posts->links()}}
+ 
+    @endsection()
+
